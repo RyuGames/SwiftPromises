eb7528332bd24009da9d506a01ab45b917285499
Wyatt Mufson <wyatt@ryucoin.com>
2019-06-16 18:28:33 -0700

    First implementation of Promises in Swift

---
+++ SwiftPromises/Classes/SwiftPromises.swift
@@ -0,0 +1,76 @@
+//
+//  SwiftPromises.swift
+//  SwiftPromises
+//
+//  Created by Wyatt Mufson on 6/16/19.
+//  Copyright © 2019 Ryu Blockchain Technologies. All rights reserved.
+//
+
+import Foundation
+
+public final class Promise<Value> {
+
+    private enum State<T> {
+        case pending
+        case resolved(T)
+        case rejected(Error)
+    }
+
+    private var state: State<Value> = .pending
+    private var callback: ((Value) -> Void)? = nil
+    private var errorCallback: ((Error) -> Void)? = nil
+    private var dispatchQueue: DispatchQueue = .global()
+
+    public init(dispatchQueue: DispatchQueue = .global(), executor: (_ resolve: @escaping (Value) -> Void, _ reject: @escaping (Error) -> Void) -> Void) {
+        self.dispatchQueue = dispatchQueue
+        executor(resolve, reject)
+    }
+
+    public func then(_ onResolved: @escaping (Value) -> Void) {
+        callback = onResolved
+    }
+
+    public func then<NewValue>(_ onResolved: @escaping (Value) -> Promise<NewValue>) -> Promise<NewValue> {
+        return Promise<NewValue> { resolve, reject in
+            then { value in
+                onResolved(value).then(resolve).catch(reject)
+            }
+        }
+    }
+
+    public func then<NewValue>(_ onResolved: @escaping (Value) -> NewValue) -> Promise<NewValue> {
+        return then { value in
+            return Promise<NewValue> { resolve, _ in
+                resolve(onResolved(value))
+            }
+        }
+    }
+
+    public func `catch`(_ onRejected: @escaping (Error) -> Void) {
+        errorCallback = onRejected
+    }
+
+    private func reject(error: Error) {
+        if updateState(to: .rejected(error)) {
+            guard let errorCallback = errorCallback else { return }
+            dispatchQueue.async {
+                errorCallback(error)
+            }
+        }
+    }
+
+    private func resolve(value: Value) {
+        if updateState(to: .resolved(value)) {
+            guard let callback = callback else { return }
+            dispatchQueue.async {
+                callback(value)
+            }
+        }
+    }
+
+    private func updateState(to newState: State<Value>) -> Bool {
+        guard case .pending = state else { return false }
+        state = newState
+        return true
+    }
+}
