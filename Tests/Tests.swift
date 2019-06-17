0d31f910955952cb91fd58e00901da74ace56bc6
Wyatt Mufson <wyatt@ryucoin.com>
2019-06-16 19:29:00 -0700

    Chaining test

--- Tests/Tests.swift
+++ Tests/Tests.swift
@@ -26,4 +26,34 @@ class Tests: XCTestCase {
 
         self.wait(for: [expectation], timeout: 10)
     }
+
+    func testChainPromises() {
+        let expectation = XCTestExpectation(description: "Test chaining promises")
+
+        func work1(_ string: String) -> Promise<String> {
+            return Promise { resolve, _ in
+                resolve(string)
+            }
+        }
+
+        func work2(_ string: String) -> Promise<Int> {
+            return Promise { resolve, _ in
+                resolve(Int(string) ?? 0)
+            }
+        }
+
+        func work3(_ number: Int) -> Int {
+            return number * number
+        }
+
+        work1("10").then { string in
+            return work2(string)
+        }.then { number in
+            return work3(number)
+        }.then { number in
+            XCTAssertEqual(number, 100)
+            expectation.fulfill()
+        }
+        self.wait(for: [expectation], timeout: 10)
+    }
 }
